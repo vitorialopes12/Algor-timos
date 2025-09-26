e32221e400a3487365247a5ee80413c711bb287a
vitorialopes12 <viitoriab1@gmail.com>
2025-09-26 19:34:37 -0300

    Create lt01_etseq06.py

---
+++ lt01_etseq06.py
@@ -0,0 +1,8 @@
+#Vitoria Bezerra Lopes
+#Receba os valores em x e y. Efetua a troca de seus valores e mostre seus conteúdos.
+
+x = input('Digite um valor para x: ')
+y = input('Digite um valor para y: ')
+
+x,y = y,x
+print('x = {x}, y = {y}')
