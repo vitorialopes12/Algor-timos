47b337b32e1866140e2a555185b79c8356a95324
vitorialopes12 <viitoriab1@gmail.com>
2025-09-26 20:11:34 -0300

    Create lt01_etseq09.py

---
+++ lt01_etseq09.py
@@ -0,0 +1,8 @@
+#Vitoria Bezerra Lopes
+#Receba 2 números inteiros. Calcule e mostre a soma dos quadrados
+
+numero1 = float(input('Digite o primeiro numero'))
+numero2 = float(input('Digite o segundo numero'))
+numero = float(input('Digite o terceiro numero'))               
+print('A soma dos quadrados de {numero1} e {numero2} é {numero1**2 + numero2**2}')
+      
