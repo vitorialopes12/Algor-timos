04ee4d718e54dcf7ba3e95ff11500d6486b8bb3e
vitorialopes12 <viitoriab1@gmail.com>
2025-09-26 20:50:33 -0300

    Create lt01_etseq13.py

---
+++ lt01_etseq13.py
@@ -0,0 +1,6 @@
+#Vitoria Bezerra Lopes
+#Receba a quantidade de alimento em quilos. Calcule e mostre quantos dias durará esse alimento sabendo que a pessoa consome 50g ao dia.
+
+quantidade_de_alimento = float(input('Digite a quantidade de alimento em quilos: '))
+print(f'Levará {(quantidade_de_alimento)*1000/50: } dias para consumir essa quantidade de alimento') 
+            
