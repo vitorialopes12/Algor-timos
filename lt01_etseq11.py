012c7cd49902ed1680738f7b9526b8005c9ba548
vitorialopes12 <viitoriab1@gmail.com>
2025-09-26 20:25:52 -0300

    Create lt01_etseq11.py

---
+++ lt01_etseq11.py
@@ -0,0 +1,5 @@
+#Vitoria Bezerra Lopes
+#Receba o raio de uma circunferencia. Calcule e mostre o comprimento da circunferencia. 
+
+raio = float(input('Digite o raio da circunferencia: '))
+print(f'O comprimento é de:{raio*2}')             
