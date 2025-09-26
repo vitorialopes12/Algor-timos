85b88c64128655f951573b734a478a4af6cdd47c
vitorialopes12 <viitoriab1@gmail.com>
2025-09-26 20:04:43 -0300

    Create lt01_etseq08.py

---
+++ lt01_etseq08.py
@@ -0,0 +1,5 @@
+#Vitoria Bezerra Lopes
+#Receba o valor de um depósito em poupança. Caalcule e mostre o valor após 1 mes de aplicaçao sabendo que rende 1,3% a.m.
+
+deposito = float(input('Digite o valor do depósito: R$'))
+print(f'A aplicaçao rendeu R${deposito*1.013:}f)
