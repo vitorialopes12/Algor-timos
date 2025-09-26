fb22a9b434af29b5e2a0041c43281f8c04b762ec
vitorialopes12 <viitoriab1@gmail.com>
2025-09-26 20:59:07 -0300

    Create lt01_etseq14.py

---
+++ lt01_etseq14.py
@@ -0,0 +1,6 @@
+#Vitoria Bezerra Lopes
+#Recebe 2 angulos de um triangulo. Calcule e mostre o valor do 3º angulo.
+
+angulo1 = float(input('Digite o valor do primeiro angulo: '))
+angulo2 = float(input('Digite o valor do segundo angulo: '))
+print(f'O valor do 3º angulo é: {180-(angulo1+angulo2)}')
