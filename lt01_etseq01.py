f32b8191afaa44d6e0f83653c8d6d73bedff4c4b
vitorialopes12 <viitoriab1@gmail.com>
2025-09-26 18:41:34 -0300

    lt01_etseq01.py

---
+++ lt01_etseq01.py
@@ -0,0 +1,9 @@
+#Vitoria Bezerra Lopes
+#Coletar o valor do lado de um quadrado, calcular sua área e apresentar o resultado.
+
+lado: float 
+area: float 
+
+lado = float(input('Digite o valor do lado do quadrado: '))
+area = lado * lado 
+print('A área do quadrado é:', area)
