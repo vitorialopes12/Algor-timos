bfc47ff4f5bfa2735b109a5c4b8966d08759f777
vitorialopes12 <viitoriab1@gmail.com>
2025-09-26 20:21:16 -0300

    Create lt01_etseq10.py

---
+++ lt01_etseq10.py
@@ -0,0 +1,6 @@
+#Vitoria Bezerra Lopes
+#Receba 2 números reais. Calcule e mostre a diferença desses valores.
+
+numero1 = int(input('Digite o primeiro número'))
+ numero2 = int(input('Digite o segundo número'))
+  print(f'A diferença entre {numero1} e {numero2} é {numero1-numero2}'f)
