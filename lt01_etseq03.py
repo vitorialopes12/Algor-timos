accad04386ec28feba527cb170b093b2ebea67d7
vitorialopes12 <viitoriab1@gmail.com>
2025-09-26 19:18:42 -0300

    Create lt01_etseq03.py

---
+++ lt01_etseq03.py
@@ -0,0 +1,6 @@
+#Vitoria Bezerra Lopes
+#Receba a base e a altura de um triangulo. Calcule e mostre a sua área.
+
+base = int(input('Digite o valor da base do triangulo: '))
+altura = int(input('Digite o valor da altura do triangulo: '))
+print('A área do triangulo é: {(base*altura)/2}')
