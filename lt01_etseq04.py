d6c5f6138453ed7b7a9977c21410e2deda90c573
vitorialopes12 <viitoriab1@gmail.com>
2025-09-26 19:22:32 -0300

    Create lt01_etseq04.py

---
+++ lt01_etseq04.py
@@ -0,0 +1,5 @@
+#Vitoria Bezerra Lopes
+#Receba a temperatura em graus Celsius. Calcule e mostre a sua temperatura convertida em fahrenheit.
+
+celsius = float(input('Digite a temperatura em graus celsius: '))
+print({celsius} é igual a {((98celsius)+160)/5}F')
