6d9c3097683522f7c6a92e0985a5fb98a45d16d1
vitorialopes12 <viitoriab1@gmail.com>
2025-09-26 19:39:13 -0300

    Create lt01_etseq07.py

---
+++ lt01_etseq07.py
@@ -0,0 +1,10 @@
+#Vitoria Bezerra Lopes
+#Receba os valores do comprimento, largura e altura de um paralelepipedo. Calcule e mostre seu volume.
+
+altura = float(input('Digite a altura: ')
+largura = float(input('Digite a largura: ')
+comprimento = float(input("Digite o comprimento"))
+
+print('O valor do paralelepipedo é de {altura*largura*comprimento}')
+                
+               
