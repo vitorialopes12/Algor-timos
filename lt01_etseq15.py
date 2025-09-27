2c9db4c7f289ea838ec53eca05bb2b29c3396ec5
vitorialopes12 <viitoriab1@gmail.com>
2025-09-26 21:07:13 -0300

    Create lt01_etseq15.py

---
+++ lt01_etseq15.py
@@ -0,0 +1,8 @@
+#Vitoria Bezerra Lopes
+#Receba os valores de 2 catetos de um triangulo retangulo. Calcule e mostre a hipotenusa. 
+
+cateto1 = float(input('Digite o valor do primeiro cateto: ')
+cateto2 = float(input('Digite o valor do segundo cateto: ')
+
+print((catetto1**2)+cateto2**2)
+print(f'A hipotenusa é de {((cateto1**2)+cateto2**2))**0.5}')                
