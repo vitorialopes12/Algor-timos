4464a94aa7ca021277f8c1f5b308c1f6cd493669
vitorialopes12 <viitoriab1@gmail.com>
2025-09-26 21:33:15 -0300

    Create lt01_etseq18.py

---
+++ lt01_etseq18.py
@@ -0,0 +1,11 @@
+#Vitoria Bezerra Lopes
+#Receba 2 valores inteiros. Calcule e mostre o resultado da diferença do maior pelo menor valor.
+
+valor1 = float(input('Digite o primeiro valor: ')
+valor2 = float(input('Digite o segundo valor: ')
+
+if valor1>valor2:
+               print(valor1-valor2)
+else:
+               print(valor2-valor1)
+            
