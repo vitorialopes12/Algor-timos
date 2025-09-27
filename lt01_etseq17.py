563aa0bda1143a99bbdcfac5b8df6cc0dd3fa553
vitorialopes12 <viitoriab1@gmail.com>
2025-09-26 21:28:46 -0300

    Create lt01_etseq17.py

---
+++ lt01_etseq17.py
@@ -0,0 +1,8 @@
+#Vitoria Bezerra Lopes
+#Calcule a quantidade de litros gastos em uma viagem, sabendo que o automovel faz 12km/l. Receber o tempo de percurso e a velocidade média.
+
+tempo = float(input('Digite o tempo do percurso viajado: ')
+veloc_media = flot(input('Digite a velocidade média da viagem: '))
+distancia_viajada = tempo*veloc_media
+
+print(f'A quantidade de gasolina gasta é de {distancia_viajada/12:}')
