ab8dd45813a5e2147752ca4806ba524bdf95722c
vitorialopes12 <viitoriab1@gmail.com>
2025-09-26 21:46:10 -0300

    Create lt01_etseq19.py

---
+++ lt01_etseq19.py
@@ -0,0 +1,10 @@
+#Vitoria Bezerra Lopes
+#Receba 2 valores reais. Calcule e mostre o maior deles.
+
+valor1 = float(input('Digite um valor: '))
+valor2 = float(input('Digite outro valor: '))
+
+if valor1>valor2:
+  print(f'O maior valor é: {valor1}')
+else:
+print(f'O maior valor é: {valor2}')
