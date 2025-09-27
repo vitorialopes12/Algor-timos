4307c4df204a1c0cd3f31813232d085c6f269192
vitorialopes12 <viitoriab1@gmail.com>
2025-09-26 22:29:09 -0300

    Create lt01_etseq22.py

---
+++ lt01_etseq22.py
@@ -0,0 +1,12 @@
+#Vitoria Bezerra Lopes
+#Receba 2 valores inteiros e diferentes. Mostre seus valores em ordem crescente.
+
+valor1 = int(input('Digite o primeiro valor: '))
+valor2 = int(input('Digite o segundo valor: '))
+
+if valor2>valor1:
+  print(f'Os valores em ordem crescente: {valor1}, {valor2}')
+else:
+  print(f'Os valores em ordem crescente: {valor2}, {valor1}')
+                                        
+                                        
