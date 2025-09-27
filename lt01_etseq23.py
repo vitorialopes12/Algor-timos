4af6728d48b9dedf6c4725eacb23e0966cfca544
vitorialopes12 <viitoriab1@gmail.com>
2025-09-26 22:54:30 -0300

    Create lt01_etseq23.py

---
+++ lt01_etseq23.py
@@ -0,0 +1,23 @@
+#Vitoria Bezerra Lopes
+#Receba 3 valores obrigatoriamente em ordem crescente e um 4º valor nao necessariamente em ordem. Mostre os 4 números em ordem crescente.
+
+valor1 = int(input('Digite o primeiro valor: '))
+valor2 = int(input('Digite o segundo valor: '))
+
+while valor2<valor1:
+  valor2 = int(input('Digite o segundo valor: '))
+valor3 = int(input('Digite o terceito valor: '))
+while valor3 = int(input('Digite o segundo valor: '))
+outro_valor = int(input('Digite um valor: '))
+
+if outro_valor>valor3:
+  print({'valor1, valor2, valor3, outro_valor'})
+
+elif outro_valor>valor2:
+  print({valor1, valor2, outro_valor, valor3})
+
+elif outro_valor>valor2
+print({valor1, outro_valor, valor2, valor3})
+
+else: 
+print({outro_valor, valor1, valor2, valor3})
