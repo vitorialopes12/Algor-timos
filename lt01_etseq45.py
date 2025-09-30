5d4b599f5dac5eb7621743a06b39bec588cf893c
vitorialopes12 <viitoriab1@gmail.com>
2025-09-30 19:09:55 -0300

    Create lt01_etseq45.py

---
+++ lt01_etseq45.py
@@ -0,0 +1,8 @@
+#Vitoria Bezerra Lopes
+#Calcule e mostre a série 1 – 2/4 + 3/9 – 4/16 + 5/25 + ... + 15/225
+
+resultado = 0
+for numero in range(1,16):
+  termo = = -termo
+  resultado +=termo
+ pritn(resultado) 
