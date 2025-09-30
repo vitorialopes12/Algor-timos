319d4fb6889721771fc73a8dfe05489c05b83890
vitorialopes12 <viitoriab1@gmail.com>
2025-09-30 18:40:32 -0300

    Create lt01_etseq39.py

---
+++ lt01_etseq39.py
@@ -0,0 +1,8 @@
+#Vitoria Bezerra Lopes
+#Calcule a quantidade de grãos contidos em um tabuleiro de xadrez onde: Casa: 	1	2	3	4	...	64 Qdte:	1	2	4	8	...	N
+
+casas = 64
+total = 1
+for i in range(casas):
+  print(total)
+  total+=2
