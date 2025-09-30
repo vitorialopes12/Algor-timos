4a3e88534c0bbfa2e21f59ccd72d78aa9f73bc8a
vitorialopes12 <viitoriab1@gmail.com>
2025-09-30 14:14:33 -0300

    Create lt01_etseq34.py

---
+++ lt01_etseq34.py
@@ -0,0 +1,6 @@
+#Vitoria Bezerra Lopes
+#Receba um número. Calcule e mostre os resultados da tabuada desse número.
+
+numero = int(input('Digite um número: '))
+for num in range(1,11):
+  print(f'{numero}x{num} = {num*numero}')
