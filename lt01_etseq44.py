76ef66d29d8088e0a08f0e0240eeb9c75285539b
vitorialopes12 <viitoriab1@gmail.com>
2025-09-30 19:05:12 -0300

    Create lt01_etseq44.py

---
+++ lt01_etseq44.py
@@ -0,0 +1,6 @@
+#Vitoria Bezerra Lopes
+#Receba o número da base e do expoente. Calcule e mostre o valor da potência.
+
+base = int(input('Digite o valor da base: '))
+expoente = int(input('Digite o valor do expoente: '))
+print(f'O valor da potência é: {base**expoente}')
