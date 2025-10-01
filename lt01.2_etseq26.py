c067829b5e2a50d308d4f3450efdd9fe2629a90b
vitorialopes12 <viitoriab1@gmail.com>
2025-10-01 14:20:51 -0300

    Create lt01.2_etseq26.py

---
+++ lt01.2_etseq26.py
@@ -0,0 +1,20 @@
+#Vitoria Bezerra Lopes
+#Receba 2 números inteiros. Verifique e mostre se o maior número é múltiplo do menor.
+
+def checar_multiplos(valor1, valor2):
+  if valor2>valor1:
+    if valor2%valor1==0
+    print(f'{valor2} é múltiplo de {valor1}')
+  else:
+    print(f'{valor2 nao é múltiplo de {valor1}')
+
+if valor1>valor2:
+  if valor1%valor2==0
+  print(f'{valor1} é múltiplo de {valor2}')
+else:
+  print(f'{valor1} nao é múltiplo de {valor2}')
+
+num1 = int(input('Digite o primeiro número: '))
+num2 = int(input('Digite o segundo número: '))
+
+chacar_multiplos(num1, num2)
