5c6e42790ea366d6c7d54e6bb44bfcec68e353a8
vitorialopes12 <viitoriab1@gmail.com>
2025-10-01 12:35:18 -0300

    Create lt01.2_etseq18.py

---
+++ lt01.2_etseq18.py
@@ -0,0 +1,13 @@
+#Vitoria Bezerra Lopes 
+#Receba 2 valores inteiros. Calcule e mostre o resultado da diferença do maior pelo mneor valor.
+
+def maior_menor(valor1, valor2)
+if valor1>valor2:
+  print(valor1-valor)
+else:
+  print(valor2-valor1)
+
+num1 =  int(input('Digite um número: '))
+num2 = int(input('Digite outro número: '))
+
+maior_menor(num1, num2)
