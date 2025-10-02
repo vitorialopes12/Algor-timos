087f0106c6539dcbcdfea85972e3a5f9a4be9888
vitorialopes12 <viitoriab1@gmail.com>
2025-10-02 14:09:34 -0300

    Create lt01.2_etseq.py

---
+++ lt01.2_etseq.py
@@ -0,0 +1,20 @@
+#Vitoria Bezerra Lopes
+#Receba 2 números inteiros, verifique qual o maior entre eles. Calcule e mostre o resultado da somatória dos números ímpares entre esses valores.
+
+def soma_impares():
+  num1 = int(input('Digite o primeiro número: '))
+  num2 = int(input('Digite o segundo número: '))
+  resultado = 0
+  if num1>num2: 
+  for numero in range(num2+1, num1):
+  if numero%2!=0:
+    resultado+=numero
+    print(resultado)
+elif num>num1:
+for numero in range(num1+1, num2):
+  if numero%2!=0:
+    resultado+=numero
+    print(resultado)
+else:
+  print('Os números sao iguais')
+  soma_impares()
