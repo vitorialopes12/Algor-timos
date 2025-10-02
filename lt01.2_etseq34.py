8f8ea9604d47dc90d91dc3b051f75ebabec1d01a
vitorialopes12 <viitoriab1@gmail.com>
2025-10-02 14:00:49 -0300

    Create lt01.2_etseq34.py

---
+++ lt01.2_etseq34.py
@@ -0,0 +1,8 @@
+#Vitoria Bezerra Lopes
+#Receba um número. Calcule e mostre os resultados da tabuada desse número.
+
+def tabuada():
+  numero = (int(input('Digite um número: '))
+            for num in range(1,11):
+               print(f'{numero} x {num} = {num*numero}'))
+  tabuada()
